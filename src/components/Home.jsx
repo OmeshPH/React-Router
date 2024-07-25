0e3a18f3dbd9cfbbed69b8373902faaccc32c70d
OmeshPH <OmeshPH@gmail.com>
2024-07-25 12:45:17 -0400

    completed files

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
-import Red from './components/Red.jsx';
-import Blue from './components/Blue.jsx';
+import Red from './components/Red';
+import Blue from './components/Blue';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <nav id="navbar">
-        <Link to="/blue"> Blue </Link>
-
-        <Link to="/red"> Red </Link>
+        <Link to="/">Home</Link>
+        <Link to="/blue">Blue</Link>
+        <Link to="/red">Red</Link>
       </nav>
       <div id="main-section">
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/blue" element={<Blue />} />
           <Route path="/red" element={<Red />} />
         </Routes>
